32eec21d2d29f8ce1a8dc8c8a80e4575974df853
kpicaza <kpicaza.sound@gmail.com>
2019-03-31 04:22:57 +0200

    clean coding style

--- src/Application/Http/Handler/ApplicationRequestHandler.php
+++ src/Application/Http/Handler/ApplicationRequestHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Antidot\Application\Http\Handler;
 
+use Antidot\Application\Http\Middleware\Pipeline;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
