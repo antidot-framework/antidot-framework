3d3cd54b6180120e61e1e9bcd33ce395efdca290
kpicaza <kpicaza.sound@gmail.com>
2020-08-20 21:25:09 +0200

    issue-16: test the error middleware factory

--- src/Container/ErrorMiddlewareFactory.php
+++ src/Container/ErrorMiddlewareFactory.php
@@ -5,13 +5,31 @@ declare(strict_types=1);
 namespace Antidot\Container;
 
 use Antidot\Application\Http\Middleware\ErrorMiddleware;
+use InvalidArgumentException;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Server\MiddlewareInterface;
+
+use function array_key_exists;
+use function gettype;
+use function is_bool;
+use function sprintf;
 
 class ErrorMiddlewareFactory
 {
     public function __invoke(ContainerInterface $container): MiddlewareInterface
     {
-        return new ErrorMiddleware($container->get('config')['debug']);
+        /** @var array<string, mixed> $config */
+        $config = $container->get('config');
+
+        return new ErrorMiddleware($this->hasDebug($config));
+    }
+
+    /**
+     * @param array<string, mixed> $config
+     * @return bool
+     */
+    private function hasDebug(array $config): bool
+    {
+        return array_key_exists('debug', $config) ? (bool) $config['debug'] : false;
     }
 }
